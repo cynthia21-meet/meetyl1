9a902c5b9acfd88e68095a48608f76a88b3fb99a
cynthia21-meet <52948777+cynthia21-meet@users.noreply.github.com>
2019-11-24 12:42:43 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -27,5 +27,23 @@ user2.post( " Life is beautiful Be Happy !!! ")
 user1.get_userInfo()
 user2.get_userInfo()
 user1.remove_friend( " jess2004@yahoo.com " )
-
+global_list=[]
+def users():
+	A = input( " what is your name ? " )
+	B = input( " what is your email ? " )
+	C = input( " what is your password ? " )
+	user3 = ( A , B , C )
+	global_list.append( user3 )
+def posts():
+	W = input( " what is your name ? " )
+	X = input( " what is your email ? " )
+	Y = input( " what is your password ? " )
+	Z = input( " what is your text ? " )
+	user4 = ( W , X , Y , Z )
+	global_list.append( user4 )
+class Comment(Post):
+	def __init__ (self,like,reply):
+		Post.__init__(self)
+		self.like = like
+		self.reply = reply
 
